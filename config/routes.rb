1f9a3f59dacbbfc1ad4ea4880c5c20d940060cea
tktezy <tkey5074@gmail.com>
2019-12-13 17:29:54 +0300

    Added gem cancancan. Improoved sign in button in header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root to: 'home#index'
+	devise_scope :user do
+		root to: 'home#index'
+		get "sign_in" => "devise/sessions#new"
+		get "sign_out" => "devise/sessions#destroy"
+	end
 
 end

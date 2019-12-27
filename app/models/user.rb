bac5f38f6fc3f394550347378f0437bfeb42b99f
tktezy <tkey5074@gmail.com>
2019-12-27 19:13:02 +0300

    small db seed fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  before_create :assign_default_rolez
+  before_create :assign_default_role
 
   def assign_default_role
-    self.role = "player"
+	if self.role == nil
+		self.role = "player"
+	end
   end
 end
